b48194c70a2a721ccfdfda6be23598d22bc8e9d7
llwwns <llwwns@gmail.com>
2016-10-30 15:51:22 +0900

    Fix jade to pug

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ global.client.on("error", function (err) {
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
